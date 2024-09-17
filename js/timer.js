54c69af62a5363bf64e51024ab42a560aebcc50e
mustaphapai <mustaphapai@gmail.com>
2024-09-17 12:03:11 +0100

    date change

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,8 @@
 // Set the date we're counting down to
-var countDownDate = new Date("September 28, 2024 15:37:25").getTime();
+var countDownDate = new Date("November 1, 2024 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
 
@@ -17,8 +16,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
